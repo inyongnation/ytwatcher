b80e3d3ea39fb4ff52f2e5625447cf7bee74c18f
InyongNation <71963003+inyongnation@users.noreply.github.com>
2023-12-17 14:22:03 +0800

    Update watch.py

--- watch.py
+++ watch.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 import time
 
 # Set up Firefox options
